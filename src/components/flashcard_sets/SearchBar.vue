aea7f0d25bbc8588a626f1984c3902ed1369afe9
Kuba Nowak <kubanowak9755@gmail.com>
2020-04-16 14:47:58 +0200

    fixed search bar styling

--- src/components/flashcard_sets/SearchBar.vue
+++ src/components/flashcard_sets/SearchBar.vue
@@ -1,15 +1,19 @@
 <template>
-  <div class="col-md-9 mx-auto">
-    <form class="form-inline my-2 my-lg-0">
-      <input
-        class="form-control w-75 mx-auto"
-        type="search"
-        placeholder="Search"
-        aria-label="Search"
-      />
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
-        Search
-      </button>
+  <div class="w-100">
+    <form class="sets-search-form">
+      <div class="w-75 row mx-auto">
+        <input
+          class="form-control col-9 w-75 mx-auto"
+          type="search"
+          placeholder="Search"
+          aria-label="Search"
+        />
+        <input
+          class="btn btn-outline-success my-2 my-sm-0 offset-1 col-2"
+          type="submit"
+          value="Search"
+        />
+      </div>
     </form>
   </div>
 </template>
