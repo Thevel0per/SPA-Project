45210111ee0c27a2cea10914d9d70ec1efa5e8ca
HiTchHiKer96 <krzysiek.paszko.96@gmail.com>
2020-05-01 19:59:24 +0200

    added translate function

--- src/components/flashcard_sets/Flashcard.vue
+++ src/components/flashcard_sets/Flashcard.vue
@@ -21,6 +21,39 @@
           />
           <div v-if="edit_set"> 
             <br>
+
+
+            <div v-if="translate" class="row justify-content-center">
+                Translate word:
+              </div>
+                <div class="row justify-content-center">
+                  <div class="dropdown">
+                    <button v-if="translate" class="mx-auto btn btn-outline-success  dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                      {{this.language.name}}
+                    </button>
+                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                      <a class="dropdown-item" @click="changeLanguage(auto_language)">Auto</a>
+                      <a v-for="(item) in supported_languages" :key="item.name" class="dropdown-item" @click="changeLanguage(item)">{{item.name}}</a>
+                    </div>
+                  </div>
+
+                  <div class="dropdown">
+                    <button v-if="translate" class="mx-auto btn btn-outline-success  dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                      {{this.target_language.name}}
+                    </button>
+                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                      <a v-for="(item) in supported_languages" :key="item.name" class="dropdown-item" @click="changeTargetLanguage(item)">{{item.name}}</a>
+                    </div>
+                  </div>
+                </div>
+              <div class="row justify-content-center">
+                <button v-if="translate" @click="translateWord()">
+                  <i class="fas fa-2x fa-check"></i>
+                </button>
+              </div>
+              <br v-if="translate">
+
+
             <div class="row justify-content-center">
               <button v-if="edit_card==false" @click="deleteFlashcard()">
                 <i class="far fa-2x fa-trash-alt"></i>
@@ -28,7 +61,10 @@
               <button v-if="edit_card" @click="changeEdit()">
                 <i class="fas fa-2x fa-times"></i>
               </button>
-             <button v-else @click="changeEdit()">
+              <button v-if="edit_card" @click="changeTranslate()">
+                  <i class="fas fa-2x fa-language"></i>
+              </button>
+             <button v-if="edit_card==false" @click="changeEdit()">
                 <i class="far fa-2x fa-edit"></i>
               </button>
               <button v-if="edit_card" @click="updateFlashcard()">
@@ -51,7 +87,17 @@ export default {
     return {
       edit_card: false,
       word: this.flashcard.word,
-      translated_word: this.flashcard.translated_word
+      translated_word: this.flashcard.translated_word,
+      translate: false,
+      language: {name: "from", code: ''},
+      target_language: {name: "to", code: ''},
+      supported_languages: [ 
+        {name: "English", code: "en"},
+        {name: "Polish", code: "pl"},
+        {name: "German", code: "de"},
+        {name: "French", code: "fr"}
+        ],
+      auto_language: {name: "Auto", code: 'auto'}
     };
   },
   methods:{
@@ -70,18 +116,56 @@ export default {
 
       this.edit_card = false;
       this.$emit("updateset");
+    },
+    changeTranslate(){
+      if(this.translate == false) this.translate = true;
+      else this.translate = false;
+    },
+    changeLanguage(language){
+      this.language = language;
+      console.log("aaa");
+    },
+    changeTargetLanguage(target_language){
+      this.target_language = target_language;
+    },
+    translateWord(){
+      this.translated_word = "Please wait";
+        if(this.language.code == 'en' || this.target_language.code == 'en'){
+        this.$root.db.flashcards.translate(this.language.code, this.target_language.code, this.word, translated_word => {
+          console.log("abc",JSON.parse(translated_word).outputs[0].output);
+          this.translated_word = JSON.parse(translated_word).outputs[0].output;
+        });
+      }
+      else {
+        this.$root.db.flashcards.translate(this.language.code, 'en', this.word, translated_en_word => {
+          console.log("abc",JSON.parse(translated_en_word).outputs[0].output);
+          this.translated_word = JSON.parse(translated_en_word).outputs[0].output;
+
+         this.$root.db.flashcards.translate('en', this.target_language.code, JSON.parse(translated_en_word).outputs[0].output, translated_word => {
+            console.log("abc",JSON.parse(translated_word).outputs[0].output);
+            this.translated_word = JSON.parse(translated_word).outputs[0].output;
+          });
+
+        });
+      }
     }
-  }
+  },
+  
 };
 </script>
 
 <style scoped>
-button,
-a {
+button {
   background: none;
   color: white;
   border: none;
   margin: 0 5px;
   width: 50px;
+}
+#dropdownMenuButton {
+  background: white;
+  color: black; 
+  margin: 10px 10px!important;
+  width: 120px;
 }
 </style>
--- src/components/flashcard_sets/FlashcardSet.vue
+++ src/components/flashcard_sets/FlashcardSet.vue
@@ -42,11 +42,47 @@
                   class="form-control"
                   placeholder="Translated word"
                   v-model="translated_word"
-                />  
+                /> 
               <br>
+
+
+              <div v-if="translate" class="row justify-content-center">
+                Translate word:
+              </div>
+                <div class="row justify-content-center">
+                  <div class="dropdown">
+                    <button v-if="translate" class="mx-auto btn btn-outline-success  dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                      {{this.language.name}}
+                    </button>
+                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                      <a class="dropdown-item" @click="changeLanguage(auto_language)">Auto</a>
+                      <a v-for="(item) in supported_languages" :key="item.name" class="dropdown-item" @click="changeLanguage(item)">{{item.name}}</a>
+                    </div>
+                  </div>
+
+                  <div class="dropdown">
+                    <button v-if="translate" class="mx-auto btn btn-outline-success  dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                      {{this.target_language.name}}
+                    </button>
+                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                      <a v-for="(item) in supported_languages" :key="item.name" class="dropdown-item" @click="changeTargetLanguage(item)">{{item.name}}</a>
+                    </div>
+                  </div>
+                </div>
+              <div class="row justify-content-center">
+                <button v-if="translate" @click="translateWord()">
+                  <i class="fas fa-2x fa-check"></i>
+                </button>
+              </div>
+              <br v-if="translate">
+
+
               <div class="row justify-content-center">
                 <button @click="addFlashcard()">
                   <i class="fas fa-2x fa-plus"></i>
+                </button>
+               <button @click="changeTranslate()">
+                  <i class="fas fa-2x fa-language"></i>
                 </button>
               </div>
             </div>
@@ -71,7 +107,17 @@ export default {
       word: '',
       translated_word: '',
       edit_set: false,
-      setname: ''
+      setname: '',
+      translate: false,
+      language: {name: "from", code: ''},
+      target_language: {name: "to", code: ''},
+      supported_languages: [ 
+        {name: "English", code: "en"},
+        {name: "Polish", code: "pl"},
+        {name: "German", code: "de"},
+        {name: "French", code: "fr"}
+        ],
+      auto_language: {name: "Auto", code: 'auto'}
     };
   },
   methods:{
@@ -87,6 +133,10 @@ export default {
       if(this.edit_set == false) this.edit_set = true;
       else this.edit_set = false;
     },
+    changeTranslate(){
+      if(this.translate == false) this.translate = true;
+      else this.translate = false;
+    },
     updateEvent(){
       this.$root.db.flashcardsSets.getSetById(this.$route.params.id ,set => {   
         this.setname = set.data().name; 
@@ -94,6 +144,34 @@ export default {
           this.flashcards = i;
         });
       });
+    },
+    changeLanguage(language){
+      this.language = language;
+      console.log("aaa");
+    },
+    changeTargetLanguage(target_language){
+      this.target_language = target_language;
+    },
+    translateWord(){
+      this.translated_word = "Please wait";
+        if(this.language.code == 'en' || this.target_language.code == 'en'){
+        this.$root.db.flashcards.translate(this.language.code, this.target_language.code, this.word, translated_word => {
+          console.log("abc",JSON.parse(translated_word).outputs[0].output);
+          this.translated_word = JSON.parse(translated_word).outputs[0].output;
+        });
+      }
+      else {
+        this.$root.db.flashcards.translate(this.language.code, 'en', this.word, translated_en_word => {
+          console.log("abc",JSON.parse(translated_en_word).outputs[0].output);
+          this.translated_word = JSON.parse(translated_en_word).outputs[0].output;
+
+         this.$root.db.flashcards.translate('en', this.target_language.code, JSON.parse(translated_en_word).outputs[0].output, translated_word => {
+            console.log("abc",JSON.parse(translated_word).outputs[0].output);
+            this.translated_word = JSON.parse(translated_word).outputs[0].output;
+          });
+
+        });
+      }
     }
   },
   mounted: function() {
@@ -108,12 +186,18 @@ export default {
 </script>
 
 <style scoped>
-button,
-a {
+button {
   background: none;
   color: white;
   border: none;
   margin: 0 5px;
   width: 50px;
 }
+#dropdownMenuButton {
+  background: white;
+  color: black; 
+  margin: 10px 10px!important;
+  width: 120px;
+}
+
 </style>
