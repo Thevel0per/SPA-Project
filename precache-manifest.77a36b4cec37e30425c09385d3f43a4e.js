158dca081632e49baba79695769b70ec509a8c7f
Kuba Nowak <kubanowak9755@gmail.com>
2020-05-03 13:55:38 +0200

    initial dist subtree commit

---
+++ precache-manifest.77a36b4cec37e30425c09385d3f43a4e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c984773f1d7bd05ea4aa",
+    "url": "/SPA-Project/css/app.d966b1c4.css"
+  },
+  {
+    "revision": "5ea20cdc025eb3937cc6fdb4040b755c",
+    "url": "/SPA-Project/index.html"
+  },
+  {
+    "revision": "c984773f1d7bd05ea4aa",
+    "url": "/SPA-Project/js/app.8e6fe19b.js"
+  },
+  {
+    "revision": "aa5c64c130ab72dc667e",
+    "url": "/SPA-Project/js/chunk-vendors.b6c25ce1.js"
+  },
+  {
+    "revision": "22495b5fb7fdbd050a74f2c6cd0e5af0",
+    "url": "/SPA-Project/main.css"
+  },
+  {
+    "revision": "7c976c57dc440c8f9165445e915e86b4",
+    "url": "/SPA-Project/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/SPA-Project/robots.txt"
+  }
+]);
